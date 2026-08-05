1a7675c9d14da70940f265225cb645e529691a8a
angelsanddevslol <angelinajiang60@gmail.com>
2026-08-04 23:22:04 -0400

    Merge main into matching-exercise

--- components/exercise/ExerciseBoard.tsx
+++ components/exercise/ExerciseBoard.tsx
@@ -7,7 +7,10 @@ import ComponentBoard from "./ComponentBoard/ComponentBoard"
 import { Box } from "@mui/material";
 import { useState } from 'react'
 import { CA_COMPONENTS } from "@/lib/ca-data";
-import { DragDropProvider } from "@dnd-kit/react"
+import { DragDropProvider } from "@dnd-kit/react";
+import { ToastContainer, toast } from 'react-toastify';
+import StandardToast from "./StandardToast/StandardToast";
+import { MdOutlineWarning } from "react-icons/md";
 
 export default function ExerciseBoard() {
   /* These state variables indicate where each draggable is and what each droppable is filled with */
@@ -15,7 +18,7 @@ export default function ExerciseBoard() {
   const [isFilled, setIsFilled] = useState(Object.fromEntries(CA_COMPONENTS.map((component, _) => [component.id, ""])));
   const [score, setScore] = useState(0);
   const [isVerified, setIsVerified] = useState(false);
-  
+
   function resetBoard(){
     setIsPlaced(Object.fromEntries(CA_COMPONENTS.map((component, _) => [component.id, ""])));
     setIsFilled(Object.fromEntries(CA_COMPONENTS.map((component, _) => [component.id, ""])));
@@ -28,65 +31,88 @@ export default function ExerciseBoard() {
     setScore(0);
   }
 
-  function checkWork(){
-    setScore(Object.entries(isFilled).filter((componentDroppable) => (componentDroppable[0] == componentDroppable[1])).length);
-    setIsVerified(true)
+  function allComponentsPlaced(): boolean {
+    return CA_COMPONENTS.every((component) => isPlaced[component.id] !== "");
   }
 
-  return (
-  <DragDropProvider
-    onDragEnd={(event) => {
-      if(event.canceled || event.operation.target?.id == null){
-        return
-      }
-      
-      if(event.operation.target?.id == "sidebar-droppable"){
-        if(isPlaced[event.operation.source?.id as string] == ""){
-          // console.log("Placed it in sidebar, but it was already in sidebar.")
-          return
-        } else {
-          // console.log("Placed it in sidebar, from a droppable.")
-          setIsFilled({...(isFilled), [isPlaced[event.operation.source?.id as string] as string] : ""})
-          setIsPlaced({...(isPlaced), [event.operation.source?.id as string] : ""})
-        }
-      } else {
-        if(isPlaced[event.operation.source?.id as string] != "" && isPlaced[event.operation.source?.id as string] == event.operation.target?.id){
-          // console.log("The button was in component, and dropped into same component.")
-          return
-        }
+  function checkWork(){
+    if (allComponentsPlaced()) {
+      setScore(Object.entries(isFilled).filter((componentDroppable) => (componentDroppable[0] == componentDroppable[1])).length);
+      setIsVerified(true);
+    } else {
+      toast(StandardToast, {
+        data: {
+          // custom props
+          Icon: MdOutlineWarning,
+          iconColor: "var(--color-warn)",
+          text: "Complete the diagram before checking your work",
+        },
+        toastId: "incomplete-diagram-warning", // prevents duplicate toasts
+        closeButton: false,
+        customProgressBar: true,
+        position: "bottom-left",
+        autoClose: 5000,
+      })
+    }
+  }
 
-        if(isPlaced[event.operation.source?.id as string] == ""){
-          if(isFilled[event.operation.target?.id as string] == ""){
-            // console.log("The button was in sidebar and dropped into empty component.")
-            setIsPlaced({...(isPlaced), [event.operation.source?.id as string] : event.operation.target?.id as string})
+
+  return <>
+    <ToastContainer  />
+    <DragDropProvider
+      onDragEnd={(event) => {
+        if(event.canceled || event.operation.target?.id == null){
+          return
+        }
+        
+        if(event.operation.target?.id == "sidebar-droppable"){
+          if(isPlaced[event.operation.source?.id as string] == ""){
+            // console.log("Placed it in sidebar, but it was already in sidebar.")
+            return
           } else {
-            // console.log("The button was placed from sidebar into component that already had a button.")
-            const oldDraggable = isFilled[event.operation.target?.id as string]
-            setIsPlaced({...(isPlaced), [oldDraggable] : "", [event.operation.source?.id as string] : event.operation.target?.id as string})
+            // console.log("Placed it in sidebar, from a droppable.")
+            setIsFilled({...(isFilled), [isPlaced[event.operation.source?.id as string] as string] : ""})
+            setIsPlaced({...(isPlaced), [event.operation.source?.id as string] : ""})
           }
-          setIsFilled({...(isFilled), [event.operation.target?.id as string] : event.operation.source?.id as string})
         } else {
-          const oldDroppable = isPlaced[event.operation.source?.id as string]
-          if(isFilled[event.operation.target?.id as string] == ""){
-            // console.log("The button was in component and placed into empty component.")
-            setIsFilled({...(isFilled), [oldDroppable] : "", [event.operation.target?.id as string] : event.operation.source?.id as string})
-            setIsPlaced({...(isPlaced), [event.operation.source?.id as string] : event.operation.target?.id as string})
+          if(isPlaced[event.operation.source?.id as string] != "" && isPlaced[event.operation.source?.id as string] == event.operation.target?.id){
+            // console.log("The button was in component, and dropped into same component.")
+            return
+          }
+
+          if(isPlaced[event.operation.source?.id as string] == ""){
+            if(isFilled[event.operation.target?.id as string] == ""){
+              // console.log("The button was in sidebar and dropped into empty component.")
+              setIsPlaced({...(isPlaced), [event.operation.source?.id as string] : event.operation.target?.id as string})
+            } else {
+              // console.log("The button was placed from sidebar into component that already had a button.")
+              const oldDraggable = isFilled[event.operation.target?.id as string]
+              setIsPlaced({...(isPlaced), [oldDraggable] : "", [event.operation.source?.id as string] : event.operation.target?.id as string})
+            }
+            setIsFilled({...(isFilled), [event.operation.target?.id as string] : event.operation.source?.id as string})
           } else {
-            // console.log("The button was placed in component that already had a button, had to swap.")
-            const oldDraggable = isFilled[event.operation.target?.id as string]
-            setIsFilled({...(isFilled), [oldDroppable] : oldDraggable, [event.operation.target?.id as string]: event.operation.source?.id as string})
-            setIsPlaced({...(isPlaced), [oldDraggable] : oldDroppable, [event.operation.source?.id as string]: event.operation.target?.id as string})
+            const oldDroppable = isPlaced[event.operation.source?.id as string]
+            if(isFilled[event.operation.target?.id as string] == ""){
+              // console.log("The button was in component and placed into empty component.")
+              setIsFilled({...(isFilled), [oldDroppable] : "", [event.operation.target?.id as string] : event.operation.source?.id as string})
+              setIsPlaced({...(isPlaced), [event.operation.source?.id as string] : event.operation.target?.id as string})
+            } else {
+              // console.log("The button was placed in component that already had a button, had to swap.")
+              const oldDraggable = isFilled[event.operation.target?.id as string]
+              setIsFilled({...(isFilled), [oldDroppable] : oldDraggable, [event.operation.target?.id as string]: event.operation.source?.id as string})
+              setIsPlaced({...(isPlaced), [oldDraggable] : oldDroppable, [event.operation.source?.id as string]: event.operation.target?.id as string})
+            }
           }
         }
-      }
-    }}
-  >
-    <Box sx={{ display: 'flex', flexDirection: 'column', height: '100%', overflow: 'hidden', flexGrow: 1}}>
-      <ExerciseBoardSubheader />
-      <ComponentBoard isFilled={isFilled} isVerified={isVerified}/>
-    </Box>
-    <Box sx={{ display: 'flex', flexDirection: 'column', height: '100%'}}>
-      <ExerciseBoardSidebar isPlaced={isPlaced} isVerified={isVerified} score={score} handleReset={resetBoard} handleRetry={retryBoard} handleCheckWork={checkWork}/>
-    </Box>
-  </DragDropProvider>
-)}
+      }}
+    >
+      <Box sx={{ display: 'flex', flexDirection: 'column', height: '100%', overflow: 'hidden', flexGrow: 1}}>
+        <ExerciseBoardSubheader />
+        <ComponentBoard isFilled={isFilled} isVerified={isVerified}/>
+      </Box>
+      <Box sx={{ display: 'flex', flexDirection: 'column', height: '100%'}}>
+        <ExerciseBoardSidebar isPlaced={isPlaced} isVerified={isVerified} score={score} handleReset={resetBoard} handleRetry={retryBoard} handleCheckWork={checkWork}/>
+      </Box>
+    </DragDropProvider>
+  </>
+}
--- components/exercise/ExerciseBoardSidebar/ExerciseBoardSidebar.tsx
+++ components/exercise/ExerciseBoardSidebar/ExerciseBoardSidebar.tsx
@@ -1,11 +1,11 @@
-'use client';
+"use client";
 // components/exercise/ExerciseBoardSidebar/ExerciseBoardSidebar.tsx
 // The sidebar of the exercise page.
-import ComponentPieces from "../../common/ComponentPieces/ComponentPieces"
-import ExerciseResultsPanel from "../../common/ExerciseResultsPanel/ExerciseResultsPanel"
-import { CA_COMPONENTS } from "@/lib/ca-data"
-import styles from "./ExerciseBoardSidebar.module.css"
-import { useDroppable } from '@dnd-kit/react'
+import ComponentPieces from "../../common/ComponentPieces/ComponentPieces";
+import ExerciseResultsPanel from "../../common/ExerciseResultsPanel/ExerciseResultsPanel";
+import { CA_COMPONENTS } from "@/lib/ca-data";
+import styles from "./ExerciseBoardSidebar.module.css";
+import { useDroppable } from "@dnd-kit/react";
 
 /*
 isPlaced: A mapping of draggable button ids to the id of the droppable area they are located in.
@@ -16,41 +16,115 @@ handleRetry: Reset the position of all of the draggable pieces.
 handleCheckWork: Shows the score and triggers a change in the sidebar.
 */
 interface ExerciseBoardSidebarProps {
-    isPlaced: Record<string, string>;
-    isVerified: boolean;
-    score: number;
-    handleReset(): void;
-    handleRetry(): void;
-    handleCheckWork(): void;
+  isPlaced: Record<string, string>;
+  isVerified: boolean;
+  score: number;
+  handleReset(): void;
+  handleRetry(): void;
+  handleCheckWork(): void;
 }
 
-export default function ExerciseBoardSidebar({ isPlaced, isVerified, score, handleReset, handleRetry, handleCheckWork } : ExerciseBoardSidebarProps){
-    // const {ref} = useDroppable({id : "sidebar-droppable"}) was removed to see if it still works directly passing ref.
+export default function ExerciseBoardSidebar({
+  isPlaced,
+  isVerified,
+  score,
+  handleReset,
+  handleRetry,
+  handleCheckWork,
+}: ExerciseBoardSidebarProps) {
+  // const {ref} = useDroppable({id : "sidebar-droppable"}) was removed to see if it still works directly passing ref.
 
-    return (isVerified ?
-    <ExerciseResultsPanel score={score} total={CA_COMPONENTS.length} incorrectHeading="Incorrect Components" onRetry={handleRetry}>
-        {CA_COMPONENTS.filter((component) => (isPlaced[component.id] != component.id)).map((component) => (<ComponentPieces key={component.id} layer={component.layer} label={component.id} inDroppable={false} isVerified={isVerified} draggable={!isVerified} buttonOutline={""}/>))}
-    </ExerciseResultsPanel>
-    :
-    (<aside className={styles['sidebar--container']}>
-        <div className={styles['pieces--container']}>
+  return (
+    <>
+      {isVerified ? (
+        <ExerciseResultsPanel
+          score={score}
+          total={CA_COMPONENTS.length}
+          incorrectHeading="Incorrect Components"
+          onRetry={handleRetry}
+        >
+          {CA_COMPONENTS.filter(
+            (component) => isPlaced[component.id] != component.id,
+          ).map((component) => (
+            <ComponentPieces
+              key={component.id}
+              layer={component.layer}
+              label={component.id}
+              inDroppable={false}
+              isVerified={isVerified}
+              draggable={!isVerified}
+              buttonOutline={""}
+            />
+          ))}
+        </ExerciseResultsPanel>
+      ) : (
+        <aside className={styles["sidebar--container"]}>
+          <div className={styles["pieces--container"]}>
             <p>PIECES</p>
-        </div>
-        <div className={styles['description--container']}>
-            <p className={styles['description-text']}>Drag each chip into the slot you think it belongs in. Drop chips back here to remove.</p>
-        </div>
-        <div className={styles['buttons--container']} ref={useDroppable({id : "sidebar-droppable"}).ref}>
-            <div className={styles['button--column']}>
-                {CA_COMPONENTS.filter((component) => (isPlaced[component.id] == "")).map((component, index) => (index % 2 == 0 && <ComponentPieces key={component.id} layer={component.layer} label={component.id} inDroppable={false} isVerified={isVerified} draggable={!isVerified} buttonOutline={""}/>))}
+          </div>
+          <div className={styles["description--container"]}>
+            <p className={styles["description-text"]}>
+              Drag each chip into the slot you think it belongs in. Drop chips
+              back here to remove.
+            </p>
+          </div>
+          <div
+            className={styles["buttons--container"]}
+            ref={useDroppable({ id: "sidebar-droppable" }).ref}
+          >
+            <div className={styles["button--column"]}>
+              {CA_COMPONENTS.filter(
+                (component) => isPlaced[component.id] == "",
+              ).map(
+                (component, index) =>
+                  index % 2 == 0 && (
+                    <ComponentPieces
+                      key={component.id}
+                      layer={component.layer}
+                      label={component.id}
+                      inDroppable={false}
+                      isVerified={isVerified}
+                      draggable={!isVerified}
+                      buttonOutline={""}
+                    />
+                  ),
+              )}
             </div>
-            <div className={styles['button--column']}>
-                {CA_COMPONENTS.filter((component) => (isPlaced[component.id] == "")).map((component, index) => (index % 2 == 1 && <ComponentPieces key={component.id} layer={component.layer} label={component.id} inDroppable={false} isVerified={isVerified} draggable={!isVerified} buttonOutline={""}/>))}
+            <div className={styles["button--column"]}>
+              {CA_COMPONENTS.filter(
+                (component) => isPlaced[component.id] == "",
+              ).map(
+                (component, index) =>
+                  index % 2 == 1 && (
+                    <ComponentPieces
+                      key={component.id}
+                      layer={component.layer}
+                      label={component.id}
+                      inDroppable={false}
+                      isVerified={isVerified}
+                      draggable={!isVerified}
+                      buttonOutline={""}
+                    />
+                  ),
+              )}
             </div>
-        </div>
-        <div className={styles['check-work-reset--container']}>
-            <button className={`btn btn--primary btn--check-work ${styles['btn--check-work']}`} onClick={handleCheckWork} disabled={CA_COMPONENTS.some((component) => (isPlaced[component.id] == ""))}>Check My Work</button>
-            <button className={`btn btn--secondary ${styles['btn--reset']}`} onClick={handleReset}>Reset</button>
-        </div>
-    </aside>
-)
-)}
+          </div>
+          <div className={styles["check-work-reset--container"]}>
+            <button
+              className={`btn btn--primary ${styles["btn--check-work"]} ${CA_COMPONENTS.some((component) => isPlaced[component.id] === "") && styles["btn--check-work--incomplete-diagram"]}`}
+              onClick={handleCheckWork}
+            >
+              Check My Work
+            </button>
+            <button
+              className={`btn btn--secondary ${styles["btn--reset"]}`}
+              onClick={handleReset}
+            >
+              Reset
+            </button>
+          </div>
+        </aside>
+      )}
+    </>
+  );
+}
